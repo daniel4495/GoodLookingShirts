9eff3a093a134943aecfc7eeb99fbe412512ad7c
Daniel Petti <dani4495@hotmail.com>
2022-03-02 00:12:31 -0300

    logo en la pagina, solucionado errores del navbar

--- goodlookingshirt/src/Components/Navigation/NavBar.jsx
+++ goodlookingshirt/src/Components/Navigation/NavBar.jsx
@@ -2,34 +2,36 @@ import React from 'react'
 
 
 const NavBar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-logo" href="#"><img src="./public/logo.png" alt="" /></a>
-    <a className="navbar-brand" href="#">GoodLooking Shirts</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Coleccion</a>
-        </li>
-        <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">Accesorios</a>       
-         </li>
-         <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">Rebajas</a>       
-         </li>
-        </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+return (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="/logo.png" width="30" alt=""/>
+        </a>
+        <a className="navbar-brand" href="#">GoodLooking Shirts</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <nav class="navbar navbar-light bg-light">
+        </nav>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Coleccion</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Accesorios</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Rebajas</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
-  )
+)
 }
 
 export default NavBar
